239fb2c61b00b4b0db165e9017658163d1cfe466
Ogura Katsuya <k-ogura@ist.osaka-u.ac.jp>
2020-05-10 14:57:43 +0900

    gulp.taskの非推奨記法を削除

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,58 @@
 // Sass configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-//const closureCompiler = require('google-closure-compiler').gulp();
+const closureCompiler = require('google-closure-compiler').gulp();
 const browsersync = require('browser-sync');
 
 const webpack = require('webpack');
 const webpackStream = require('webpack-stream');
 const webpackConfig = require('./webpack.config.js');
 
-gulp.task('sass', function() {
+function compileSass () {
     return gulp.src('./sass/*.scss')
             .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
             .pipe(gulp.dest('./assets/css'))
-});
-gulp.task('compile-js', function() {
+}
+
+function compileJS () {
     return gulp.src('./ts/*.ts', {base: './'})
             .pipe(webpackStream(webpackConfig, webpack))
-            /*
+            .pipe(gulp.dest('./assets/js'));
+}
+
+function optimizeJS () {
+    return gulp.src('./assets/js/output.js', {base: './'})
             .pipe(closureCompiler({
                 compilation_level: 'SIMPLE',
                 warning_level: 'QUIET',
                 language_in: 'ECMASCRIPT_2015',
                 language_out: 'ECMASCRIPT_2015',
                 js_output_file: 'output.min.js'
-                }, {
+            }, {
                 platform: ['native', 'java', 'javascript']
-                }))
-            */
+            }))
             .pipe(gulp.dest('./assets/js'));
-});
+}
 
-gulp.task('build-server', function (done) {
+function buildServer (done) {
     browsersync.init({
         server: {
             baseDir: "./",
         }
     });
     done();
-});
+}
 
-gulp.task('browser-reload', function (done){
+function browserReload (done){
     browsersync.reload();
     done();
-});
+}
 
-gulp.task('watch', function() {
-    gulp.watch('./*.html', gulp.series('browser-reload'));
-    gulp.watch('./sass/*.scss', gulp.series('sass', 'browser-reload'));
-    gulp.watch('./ts/*.ts', gulp.series('compile-js', 'browser-reload'));
-});
+function watch () {
+    gulp.watch('./*.html', gulp.series(browserReload));
+    gulp.watch('./sass/*.scss', gulp.series(compileSass, browserReload));
+    gulp.watch('./ts/*.ts', gulp.series(compileJS, browserReload));
+}
 
-const defaultTasks = gulp.series('build-server', 'watch');
+const defaultTasks = gulp.series(buildServer, watch);
 gulp.task('default', defaultTasks);
